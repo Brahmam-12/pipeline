8b7cf6e278f03332b3344b63a030e21cdb23080a
Brahmam-12 <98756307+Brahmam-12@users.noreply.github.com>
2025-07-04 10:18:31 +0530

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const http = require('http')
-const server = http.createServer((req, res)=>{
-	res.end('Hello from Docker + GitHub Actions CI/CD!');
-})
-
-server.listen(3000, ()=>{
-	console.log('Server running at http://localhost:3000');
-});
