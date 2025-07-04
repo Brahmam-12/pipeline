564ae7e7ea3104bbe0373dd48d3a93b9734df77b
Brahmam-12 <98756307+Brahmam-12@users.noreply.github.com>
2025-07-04 12:07:16 +0530

    ci/cd

--- app.js
+++ app.js
@@ -12,6 +12,14 @@ app.get('/data', (req, res) => {
   });
 });
 
+app.get('/users', (req, res) => {
+  res.json([
+    { id: 1, name: "Alice" },
+    { id: 2, name: "Bob" },
+    { id: 3, name: "Charlie" }
+  ]);
+});
+
 app.listen(8080, () => {
   console.log("Server is running on port 8080 for CI/CD pipeline!");
 });
